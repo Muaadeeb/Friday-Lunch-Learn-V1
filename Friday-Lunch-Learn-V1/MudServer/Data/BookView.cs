ceacb951f5e1ebbf084b7b250305c3ca457be1da
Timothy Ingledue <timothy.ingledue@orion.com>
2023-08-18 14:06:31 -0500

    Routing, LifeCycle, Data Binding,

---
+++ Friday-Lunch-Learn-V1/MudServer/Data/BookView.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MudServer.Data
+{
+    public class BookView
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title")]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter the author's first name")]
+        public string AuthorFirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter the author's last name")]
+        public string AuthorLastName { get; set; } = null!;
+
+        public int PrintLength { get; set; }
+
+        public string Publisher { get; set; } = null!;
+
+        public string ISBN { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Rate between 1 (low) and 10 (high)")]
+        public int ReviewRating { get; set; }
+
+        public string Comments { get; set; } = null!;
+        public bool IsActive { get; set; }
+
+        public double Price { get; set; }
+    }
+}
